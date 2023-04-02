234a14eaa7ac2a16d3c76216b1928000e9e6c12a
Timothy <todongoonditi@gmail.com>
2023-04-02 14:13:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(Word) {
+  str = Word.toLowerCase().replace(/[^a-z0-9]/g, '');
+
+  return str === Word.split('').reverse().join('');
 }
 
+console.log(isPalindrome('abba')); // true
+console.log(isPalindrome('A man, a plan, a canal: Panama')); // true
+console.log(isPalindrome('hello')); // false
+
+
 /* 
-  Add your pseudocode here
-*/
+// reverse copyInput
+// compare copyInput to Input
+// return true or false*/
 
 /*
   Add written explanation of your solution here
+  // Convert the string to lowercase and remove non-alphanumeric characters
+  // Check if the string is the same forwards and backwards
+  // Test the function with some examples
 */
 
 // You can run `node index.js` to view these console logs
